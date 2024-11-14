47fa4f0be289663dbae201de0f9185ac3b20efc9
ved-intelliteam <ved@intelliteam.ai>
2024-11-14 22:05:16 +0530

    Update settings.py

--- avi/settings.py
+++ avi/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-lqmjgdpk0x8#a=cz+=13to%=qw!!g%d*0b8xxy70b%_nh4h)5s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["43.204.229.148", "avichopra.com", "www.avichopra.com"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -117,9 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICDIRS = os.path.join(BASE_DIR, "home", "static")
+STATIC_URL = "/static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR, "home", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Enable WhiteNoise's GZip and Brotli compression
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+WHITENOISE_MAX_AGE = 31536000  # Cache static files for one year
